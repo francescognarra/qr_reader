afcad30255ec5cc703346fccece6636544d89747
Francesco Gnarra <francesco.gnarra@gmail.com>
2018-04-06 16:47:31 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Toast } from '@ionic-native/toast';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  products: any[] = [];
+  selectedProduct: any;
+  productFound:boolean = false;
+
+  constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner, private toast: Toast) {
 
   }
+
+  scan() {
+  this.selectedProduct = {};
+  this.barcodeScanner.scan().then((barcodeData) => {
+    this.productFound = true;
+    console.log(barcodeData.text);
+    alert(barcodeData.text);
+    this.selectedProduct = barcodeData.text;
+    // this.selectedProduct = this.products.find(product => product.plu === barcodeData.text);
+  }, (err) => {
+    this.toast.show(err, '5000', 'center').subscribe(
+      toast => {
+        console.log(toast);
+      }
+    );
+  });
+}
 
 }
